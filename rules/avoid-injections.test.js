a41eccc7c421687dbb5a98db91a9e5af1d50cf0e
Anton Niklasson <anton@finhack.se>
2020-12-11 14:38:46 +0100

    lint things

--- rules/avoid-injections.test.js
+++ rules/avoid-injections.test.js
@@ -1,20 +1,20 @@
-const { RuleTester } = require('eslint');
-const rule = require('./avoid-injections');
+const { RuleTester } = require("eslint")
+const rule = require("./avoid-injections")
 
-const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } })
 
 function invalidCase(code, errorIds = []) {
   return {
     code,
     errors: errorIds.map((id) => ({ messageId: id })),
-  };
+  }
 }
 
-ruleTester.run('avoid-injections', rule, {
+ruleTester.run("avoid-injections", rule, {
   valid: [
     "knex.raw('select ? from users', ['email'])",
     "knex.raw('select * from users');",
-    'knex.raw(`select * from users`)',
+    "knex.raw(`select * from users`)",
     "knex.raw('select ? from users', ['email'])",
     `const query = 'SELECT * FROM users'; const result = knex.raw(query);`,
 
@@ -24,17 +24,22 @@ ruleTester.run('avoid-injections', rule, {
     "const joinCondition = `blog_posts ON users.id = blog_posts.author`; knex('users').select(['email']).joinRaw(joinCondition)",
   ],
   invalid: [
-    invalidCase('knex.raw(`select * from ${table}`);', ['avoid-raw']),
-    invalidCase('knex.raw("select * from " + table);', ['avoid-raw']),
+    invalidCase("knex.raw(`select * from ${table}`);", ["avoid-raw"]),
+    invalidCase('knex.raw("select * from " + table);', ["avoid-raw"]),
 
-    invalidCase("knex('users').whereRaw(`id = ${id}`);", ['avoid-whereraw']),
-    invalidCase("knex('users').whereRaw(`id = ` + id);", ['avoid-whereraw']),
-    invalidCase("knex('users').whereRaw(`id = ${getId()}`);", ['avoid-whereraw']),
-
-    invalidCase("knex('users').whereRaw(`id = ${getId()}`);", ['avoid-whereraw']),
-
-    invalidCase("knex('users').select(['email']).joinRaw(`blog_posts ON users.id = ${userId}`)", [
-      'avoid-joinraw',
+    invalidCase("knex('users').whereRaw(`id = ${id}`);", ["avoid-whereraw"]),
+    invalidCase("knex('users').whereRaw(`id = ` + id);", ["avoid-whereraw"]),
+    invalidCase("knex('users').whereRaw(`id = ${getId()}`);", [
+      "avoid-whereraw",
     ]),
+
+    invalidCase("knex('users').whereRaw(`id = ${getId()}`);", [
+      "avoid-whereraw",
+    ]),
+
+    invalidCase(
+      "knex('users').select(['email']).joinRaw(`blog_posts ON users.id = ${userId}`)",
+      ["avoid-joinraw"],
+    ),
   ],
-});
+})

32d720b521bb57d6a119d93a42c0fb25616c6ad8
Anton Niklasson <anton@finhack.se>
2021-03-12 21:55:12 +0100

    feat: configure knex 'client' names through settings

--- rules/avoid-injections.test.js
+++ rules/avoid-injections.test.js
@@ -1,40 +1,44 @@
 const { RuleTester } = require("eslint");
 const rule = require("./avoid-injections");
 
-const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2015 } });
-
-function invalidCase(code, errors = []) {
-  return {
-    code,
-    errors,
-  };
+function invalidCase(code, errors = [], others = {}) {
+  return Object.assign(
+    {
+      code,
+      errors,
+    },
+    others,
+  );
 }
 
-ruleTester.run("avoid-injections", rule, {
+const tester = new RuleTester({
+  parserOptions: { ecmaVersion: 2015 },
+});
+
+tester.run("avoid-injections", rule, {
   valid: [
     "knex.raw('select ? from users', ['email'])",
-    "knex.raw('select * from users');",
     "knex.raw(`select * from users`)",
     "knex.raw('select ? from users', ['email'])",
     `const query = 'SELECT * FROM users'; const result = knex.raw(query);`,
     `
     const query = \`now() + interval '123 seconds'\`;
     function run() {
-      return knex.raw(query);
+    return knex.raw(query);
     }
     `,
     "knex('users').whereRaw('id = ?', [1]);",
     "knex('users').whereRaw(`id = 1`);",
     "const joinCondition = `blog_posts ON users.id = blog_posts.author`; knex('users').select(['email']).joinRaw(joinCondition)",
-    `
-    function sharp() {
-      return {
-        raw: () => {},
-      };
-    }
-    sharp().raw();
-    `,
-    "page.drawText(`${foo}bar`);",
+    `function sharp() { return { raw: () => {}, }; } sharp().raw();`,
+    {
+      code: "knex('users').whereRaw(`id = ` + id);",
+      settings: {
+        knex: {
+          builderName: /(transaction|trx)/i,
+        },
+      },
+    },
   ],
   invalid: [
     // .raw()
@@ -67,6 +71,18 @@ ruleTester.run("avoid-injections", rule, {
     invalidCase(
       "knex('users').select(['email']).joinRaw(`blog_posts ON users.id = ${userId}`)",
       [{ messageId: "avoid", data: { query: "joinRaw" } }],
+    ),
+
+    invalidCase(
+      "lorem.raw(`select * from ${table}`);",
+      [{ messageId: "avoid", data: { query: "raw" } }],
+      {
+        settings: {
+          knex: {
+            builderName: /lorem/i,
+          },
+        },
+      },
     ),
   ],
 });

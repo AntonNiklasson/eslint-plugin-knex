5b505f44da6e04a98d17d0e988cda51cbc23ef59
Anton Niklasson <anton@finhack.se>
2021-03-18 14:00:36 +0100

    infra: first iteration of smoke test (#10)

---
+++ smoke-test/eslint-remote-tester.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /** Repositories to scan */
+  repositories: [
+    "knex/knex",
+    "bookshelf/bookshelf",
+    "gothinkster/koa-knex-realworld-example",
+    "robmclarty/knex-express-project-sample",
+    "sheerun/knex-migrate",
+  ],
+
+  /** Extensions of files under scanning */
+  extensions: [".js", ".jsx", ".ts", ".tsx"],
+
+  /** Optional pattern used to exclude paths */
+  pathIgnorePattern: `(${[
+    "node_modules",
+    "\\/\\.", // Any file or directory starting with dot, e.g. ".git"
+    "test-results",
+    "tests",
+    "docs",
+  ].join("|")})`,
+
+  /** Optional array of rules used to filter out results. Use undefined or empty array when ESLint crashes are the only interest */
+  rulesUnderTesting: [],
+
+  /** Maximum amount of tasks ran concurrently */
+  concurrentTasks: 2,
+
+  /** ESLint configuration */
+  eslintrc: {
+    root: true,
+    plugins: ["knex"],
+    rules: {
+      "knex/avoid-injections": 2,
+    },
+  },
+
+  CI: false,
+};
